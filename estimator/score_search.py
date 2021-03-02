6627d77f59a03c24849d256acb1774e6c0cf35d5
Howard <934303306@qq.com>
2021-03-02 21:23:07 +0800

    怎加state_counts方法

--- estimator/score_search.py
+++ estimator/score_search.py
@@ -12,6 +12,9 @@ class Score_function():
     def __init__(self, data: pd.DataFrame):
         self.data = data
         self.variables = list(data.columns.values)
+        self.state_names = dict()
+        for var in self.variables:
+            self.state_names[var]=self.collect_state_names(var)
 
     def collect_state_names(self,variable):
         """收集数据中某个变量variable的状态名
@@ -19,3 +22,45 @@ class Score_function():
         """
         states = sorted(list(self.data.loc[:,variable].unique()))
         return states
+    def state_counts(self,variable,parents=[]):
+        """
+        计算该变量的状态数，得到一个交叉表。例如
+        variable = 'C', parents=['A', 'B']
+        A  a1      a2
+        B  b1  b2  b1  b2
+        C
+        c1  1   1   0   0
+        c2  0   0   1   0
+        :param variable:
+        :param parents:
+        :return:
+        """
+
+        if not parents:
+            state_count_data = self.data.loc[:,variable].value_counts()
+            state_counts = (
+                state_count_data.reindex(self.state_names[variable])
+                    .fillna(0)
+                    .to_frame()
+            )
+        else:
+            parents_states = [self.state_names[parent] for parent in parents]
+            # count how often each state of 'variable' occured, conditional on parents' states
+            state_count_data = (
+                self.data.groupby([variable] + parents).size().unstack(parents)
+            )
+            if not isinstance(state_count_data.columns, pd.MultiIndex):
+                state_count_data.columns = pd.MultiIndex.from_arrays(
+                    [state_count_data.columns]
+                )
+
+            # reindex rows & columns to sort them and to add missing ones
+            # missing row    = some state of 'variable' did not occur in data
+            # missing column = some state configuration of current 'variable's parents
+            #                  did not occur in data
+            row_index = self.state_names[variable]
+            column_index = pd.MultiIndex.from_product(parents_states, names=parents)
+            state_counts = state_count_data.reindex(
+                index=row_index, columns=column_index
+            ).fillna(0)
+        return state_counts
--- test/score_search_test.py
+++ test/score_search_test.py
@@ -1,7 +1,10 @@
 from estimator.score_search import *
 import pandas as pd
 
-data = pd.DataFrame(data=[1,2,3,4])
+data = pd.DataFrame(data={'A': ['a1', 'a1', 'a2'],
+                          'B': ['b1', 'b2', 'b1'],
+                          'C': ['c1', 'c1', 'c2']})
 print(data)
 sf = Score_function(data)
-print(sf.collect_state_names(variable=0))
+print(sf.collect_state_names(variable='A'))
+print(sf.state_counts('A',parents=['B']))
